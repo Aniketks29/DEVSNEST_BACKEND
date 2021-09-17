97849b29989302f44111b9bf6fb9a730db63a385
Aniket Kumar Singh <aniketks321@gmail.com>
2021-09-17 11:42:08 +0530

    Day 9 files updated THA

--- Day_09/Express_Generator/routes/users.js
+++ Day_09/Express_Generator/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const path = require("path");
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("Enter query /query/<your query> (quote,meme,anime) in the URL");
+});
+
+router.get("/query/:search", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../", "public/images", req.params.search + ".jpeg")
+  );
 });
 
 module.exports = router;
